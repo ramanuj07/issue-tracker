cfda864e4ee3ec364da64c0c2f46999f7ec74f76
Ramanuj <ramanuj0705@gmail.com>
2024-03-11 15:09:17 +0530

    Added spinner && wrapping up

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import Spinner from "@/components/Spinner";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface IssueForm {
@@ -11,29 +12,46 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
   const router = useRouter();
   const { register, handleSubmit } = useForm<IssueForm>();
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <form
-      className="max-w-xl p-4 space-y-4"
-      onSubmit={handleSubmit(async (data) => {
-        await fetch("/api/issues", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <TextArea placeholder="Description" {...register("description")} />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="p-4 space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            setIsLoading(true);
+            await fetch("/api/issues", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(data),
+            });
+            router.push("/issues");
+            setIsLoading(false);
+          } catch (error) {
+            setError("An unexpected error occurred.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <TextArea placeholder="Description" {...register("description")} />
+        <Button disabled={isLoading}>
+          Submit New Issue {isLoading && <Spinner />}
+        </Button>
+      </form>
+    </div>
   );
 };
 
