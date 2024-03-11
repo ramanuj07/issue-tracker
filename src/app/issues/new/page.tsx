916c6b6aebfeb6cb74d90ca1a826d3cf7ab62a3b
Ramanuj <ramanuj0705@gmail.com>
2024-03-11 13:53:59 +0530

    form submissions using react-hook-form

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,17 +1,39 @@
 "use client";
 
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl p-4 space-y-4">
+    <form
+      className="max-w-xl p-4 space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await fetch("/api/issues", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        router.push("/issues");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <TextArea placeholder="Description" {...register("description")} />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
